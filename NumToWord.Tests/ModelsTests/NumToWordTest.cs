252b230e6c41def7eef5fba23ce495b492e227db
Colin Bates and Nick Rogers <nick@>
2018-06-28 10:47:35 -0700

    Create Initial Project Folder.

---
+++ NumToWord.Tests/ModelsTests/NumToWordTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumToWord;
+using System;
+using System.Collections.Generic;
+
+namespace NumToWord.Tests
+{
+  [TestClass]
+  public class NumToWordTest
+  {
+    [TestMethod]
+    public void ExampleTest_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
---
+++ NumToWord/Models/NumToWord.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace NumToWord
+{
+public class NumToWord
+  {
+  public bool ExampleTest(int year)
+    {
+      // logic will go here
+      return false;
+    }
+  }
+}
